d65e9721be3fa05aa110552c54774d5dded422b7
hjuarez <23268165+whyrll@users.noreply.github.com>
2019-02-05 13:32:10 -0800

    Create ArrayLinearList class and add blank inherited methods

---
+++ src/data_structures/ArrayLinearList.java
@@ -0,0 +1,97 @@
+package data_structures;
+
+import java.util.Iterator;
+
+public class ArrayLinearList<T> implements LinearListADT<T> {
+
+	@Override
+	public void ends() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean addFirst(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean addLast(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T removeFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T removeLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(T obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peekFirst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T peekLast() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean contains(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public T find(T obj) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFull() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
