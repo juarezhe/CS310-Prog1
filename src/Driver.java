0fb421165bd8acfa629f439cca9e49750cbb13cf
hjuarez <23268165+whyrll@users.noreply.github.com>
2019-03-05 10:21:28 -0800

    Back-test peekLast

--- src/Driver.java
+++ src/Driver.java
@@ -1,17 +1,140 @@
 import data_structures.*;
 
 public class Driver {
-	private static final int LIST_SIZE = 10;
+	private static final int LIST_SIZE = 3;
 	private static LinearListADT<Integer> list;
 	private static final int TEST_LOOP_COUNT = LIST_SIZE + 1;
+	private static Integer INTEGER_TO_ADD = 10;
 
 	public static void main(String[] args) {
 		System.out.println("Requested new list of size " + LIST_SIZE);
 		list = new ArrayLinearList<Integer>(LIST_SIZE);
 		System.out.println("Successfully created a new list");
-		verboseTest(list);
-		runTests(list);
-		iterTest(list);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedAddFirst(INTEGER_TO_ADD);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedAddFirst(INTEGER_TO_ADD + 5);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedAddFirst(INTEGER_TO_ADD + 10);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedRemoveLast();
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedAddFirst(INTEGER_TO_ADD + 15);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		modifiedRemove(INTEGER_TO_ADD + 5);
+		modifiedPeekFirst();
+		modifiedPeekLast();
+		iterate();
+		// verboseTest(list);
+		// runTests(list);
+		// iterTest(list);
+	}
+	
+	public static void modifiedPeekFirst() {
+		Integer result = list.peekFirst();
+		
+		System.out.print("peekFirst result:\t");
+		if (result == null && list.isEmpty())
+			System.out.println("list empty");
+		else if (result != null && !list.isEmpty())
+			System.out.println(result + " successfully found");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedPeekLast() {
+		Integer result = list.peekLast();
+		
+		System.out.print("peekLast result:\t");
+		if (result == null && list.isEmpty())
+			System.out.println("list empty");
+		else if (result != null && !list.isEmpty())
+			System.out.println(result + " successfully found");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedAddFirst(Integer obj) {
+		boolean result = list.addFirst(obj);
+		
+		System.out.print("addFirst result:\t");
+		if (result == true)
+			System.out.println(obj + " successfully added");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedAddLast(Integer obj) {
+		boolean result = list.addLast(obj);
+		
+		System.out.print("addLast result:\t\t");
+		if (result == true)
+			System.out.println(obj + " successfully added");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedRemoveFirst() {
+		Integer result = list.removeFirst();
+		
+		System.out.print("removeFirst result:\t");
+		if (result == null && list.isEmpty())
+			System.out.println("list empty");
+		else if (result != null)
+			System.out.println(result + " successfully removed");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedRemoveLast() {
+		Integer result = list.removeLast();
+		
+		System.out.print("removeLast result:\t");
+		if (result == null && list.isEmpty())
+			System.out.println("list empty");
+		else if (result != null)
+			System.out.println(result + " successfully removed");
+		else
+			System.out.println("not sure what happened...");
+	}
+	
+	public static void modifiedRemove(Integer obj) {
+		Integer result = list.remove(obj);
+		
+		System.out.print("remove result:\t\t");
+		if (result == null && list.isEmpty())
+			System.out.println("list empty");
+		else if (result != null)
+			System.out.println(result + " successfully removed");
+		else
+			System.out.println("not sure what happened...");		
+	}
+	
+	public static void iterate() {
+		int itemsInLine = 0;
+		
+		System.out.println("Should print " + list.size() + " elements from " + list.peekFirst() + " to " + list.peekLast() + ":");
+		for (Integer node : list) {
+			if (itemsInLine == 50) {
+				System.out.println();
+				itemsInLine = 0;
+			}
+			System.out.print(node + ", ");
+			itemsInLine++;
+		}
+		System.out.println();
 	}
 
 	/*
